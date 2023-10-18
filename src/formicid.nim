b2e46d444b9cf53d9e42be835a0e639bde496a21
Pierre Carrier <pc@rrier.ca>
2023-10-18 23:11:38 +0000

    progress?

--- src/formicid.nim
+++ src/formicid.nim
@@ -1,4 +1,6 @@
+from strutils import parseHexInt
 from strformat import `&`
+from algorithm import reversed
 
 proc emscripten_run_script(code: cstring) {.header: "<emscripten.h>",
     importc: "emscripten_run_script".}
@@ -18,8 +20,14 @@ proc eval_js(code: string) = emscripten_run_script(cstring(code))
 
 proc eval_stor(code: cstring) {.exportc,
     codegenDecl: "__attribute__((used)) $# $#$#".} =
-  let prefix = &"I don't know how to evaluate <tt>{js_string($code)}</tt> yet. Or anything, for that matter.<br/><i>This message delivered by Nim code running in WebAssembly in a WebWorker "
-  for i in 0..high(int):
-    eval_js(&"self.postMessage('document.body.innerHTML = {prefix.js_string}.concat({i}).concat(\" times.</i>\")')")
-
-echo "Booted."
+  let codeStr = $code
+  var v = ""
+  for i in countdown(codeStr.len - 1, 0):
+    if codeStr[i] in  {'0'..'9', 'a'..'f'}:
+      v.add(codeStr[i])
+    else:
+      break
+  var round = if v.len > 0: parseHexInt(cast[string](v.reversed)) else: 0
+  let newCode = codeStr[0 ..< (codeStr.len - v.len)] & &" {round + 1:x}"
+  let prefix = &"Can't evaluate <tt>{codeStr.js_string}</tt> (or anything yet).<br/><i>— Nim code running in wasm in a WebWorker after 0x"
+  eval_js(&"self.postMessage('$target.innerHTML = {prefix.js_string}.concat(\\'{round:x}\\').concat(\" window roundtrips.</i>\");$worker.postMessage({newCode.js_string})')")
